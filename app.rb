a79e10c41135346d60f68935a749bb3833ded30d
David Salgado <david@digitalronin.com>
2020-07-18 13:46:57 +0100

    Add code to display real kafka messages
    In theory, this should display messages from a
    kafka instance, provided the right environment
    variables are set.

--- app.rb
+++ app.rb
@@ -4,20 +4,44 @@ require "bundler/setup"
 require "sinatra"
 require 'tempfile'
 
+KAFKA_TOPIC = ENV.fetch('KAFKA_TOPIC', 'messages')
+GROUP_ID = ENV.fetch('KAFKA_CONSUMER_GROUP', 'heroku-kafka-demo')
+
+def with_prefix(name)
+  "#{ENV['KAFKA_PREFIX']}#{name}"
+end
+
 def initialize_kafka
+  tmp_ca_file = Tempfile.new('ca_certs')
+  tmp_ca_file.write(ENV.fetch('KAFKA_TRUSTED_CERT'))
+  tmp_ca_file.close
+
+  consumer_kafka = Kafka.new(
+    seed_brokers: ENV.fetch('KAFKA_URL'),
+    ssl_ca_cert_file_path: tmp_ca_file.path,
+    ssl_client_cert: ENV.fetch('KAFKA_CLIENT_CERT'),
+    ssl_client_cert_key: ENV.fetch('KAFKA_CLIENT_CERT_KEY'),
+    ssl_verify_hostname: false,
+  )
+
+  $consumer = consumer_kafka.consumer(group_id: with_prefix(GROUP_ID))
+  $recent_messages = []
+
   start_consumer
 
   at_exit do
-    puts "at_exit...."
+    tmp_ca_file.unlink
   end
 end
 
 def start_consumer
   Thread.new do
+    $consumer.subscribe(with_prefix(KAFKA_TOPIC))
     begin
-      while true
-        puts "#{Time.now}: consume...."
-        sleep 3
+      $consumer.each_message do |message|
+        $recent_messages << message
+        $recent_messages.shift if $recent_messages.length > 10
+        puts "consumer received message! local message count: #{$recent_messages.size} offset=#{message.offset}"
       end
     rescue Exception => e
       puts 'CONSUMER ERROR'
@@ -32,5 +56,5 @@ get "/" do
 end
 
 get "/messages" do
-  erb :messages, locals: { messages: [] }
+  erb :messages, locals: { recent_messages: $recent_messages }
 end
